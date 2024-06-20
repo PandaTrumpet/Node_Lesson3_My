bdf478f46f30fcf18da2e34a1efabb29edd4d038
PandaTrumpet <145181488+PandaTrumpet@users.noreply.github.com>
2024-06-20 18:18:38 +0300

    update

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,21 +1,28 @@
 import { getAllStudents, getStudentById } from '../service/students.js';
-export const getStudentsController = async (req, res) => {
-  const students = await getAllStudents();
-  res.json({
-    status: 200,
-    data: students,
-    message: 'Successfully found students!',
-  });
+export const getStudentsController = async (req, res, next) => {
+  try {
+    const students = await getAllStudents();
+    res.json({
+      status: 200,
+      data: students,
+      message: 'Successfully found students!',
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getStudentByIdController = async (req, res) => {
+export const getStudentByIdController = async (req, res, next) => {
   const { studentId } = req.params;
   const student = await getStudentById(studentId);
+  // if (!student) {
+  //   res.status(404).json({ message: 'Student not found' });
+  //   return;
+  // }
   if (!student) {
-    res.status(404).json({ message: 'Student not found' });
+    next(new Error('Student not found'));
     return;
   }
-
   res.json({
     status: 200,
     data: student,
--- src/routers/students.js
+++ src/routers/students.js
@@ -1,23 +1,11 @@
 import { Router } from 'express';
-import { getAllStudents, getStudentById } from '../service/students.js';
-
+// import { getAllStudents, getStudentById } from '../service/students.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import {
+  getStudentByIdController,
+  getStudentsController,
+} from '../controllers/students.js';
 const router = Router();
-router.get('/students', async (req, res) => {
-  const students = await getAllStudents();
-  res.status(200).json({
-    data: students,
-  });
-});
-router.get('/students/:studentId', async (req, res) => {
-  const { studentId } = req.params;
-  const student = await getStudentById(studentId);
-  if (!student) {
-    res.status(404).json({
-      message: 'Student not found',
-    });
-  }
-  res.status(200).json({
-    data: student,
-  });
-});
+router.get('/students', ctrlWrapper(getStudentsController));
+router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
 export default router;
